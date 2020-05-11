42decb71c4c7692a786cc5b24f879e66794d700b
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-05-11 10:39:27 +0300

    Logic update

--- src/tic/tac/toe/TicTacToe.java
+++ src/tic/tac/toe/TicTacToe.java
@@ -17,54 +17,54 @@ public class TicTacToe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-                for (int i = 0; i < 50; i++) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("0 - pabaiga");
-        System.out.println("1 - akmuo");
-        System.out.println("2 - popierius");
-        System.out.println("3 - zirkles");
-        int choose = sc.nextInt();
-        
-        int kp = ((int) Math.random() * 3) + 1;
-            if(choose == 0){
-                kp = 0;
-                break;
-            }
-            if(choose > 3){
-                System.out.println("Blogas skaicius, tikrinkite skaiciu "+choose+".");
-            }
-        System.out.println(choose);
-        System.out.println(kp);
+        for (int i = 0; i < 50; i++) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("0 - pabaiga");
+            System.out.println("1 - akmuo");
+            System.out.println("2 - popierius");
+            System.out.println("3 - zirkles");
+            int choose = sc.nextInt();
 
-            if(choose == kp && choose != 0 && kp != 0){
-                System.out.println("Lygiosios");
-                System.out.println("----------------------------");
+        double dp = (Math.random() * 3) + 1;
+        double kp = Math.floor(dp);
+                if(choose == 0){
+                    kp = 0;
+                    break;
+                }
+                if(choose > 3){
+                    System.out.println("Blogas skaicius, tikrinkite skaiciu "+choose+".");
+                }
+            System.out.println(choose);
+            System.out.println(kp);
+
+                if(choose == kp & choose != 0 && kp != 0){
+                    System.out.println("Lygiosios");
+                    System.out.println("----------------------------");
+                }
+                if(choose == 1 & kp == 2){
+                    System.out.println("Laimejo popierius");
+                    System.out.println("----------------------------");
+                }
+                if(choose == 1 & kp == 3){
+                    System.out.println("Laimejo akmuo");
+                    System.out.println("----------------------------");
+                }
+                if(choose == 2 & kp == 1){
+                    System.out.println("Laimejo popierius");
+                    System.out.println("----------------------------");
+                }
+                if(choose == 2 & kp == 3){
+                    System.out.println("Laimejo zirkles");
+                    System.out.println("----------------------------");
+                }
+                if(choose == 3 & kp == 1){
+                    System.out.println("Laimejo akmuo");
+                    System.out.println("----------------------------");
+                }
+                if(choose == 3 & kp == 2){
+                    System.out.println("Laimejo zirkles");
+                    System.out.println("----------------------------");
+                }
             }
-            if(choose == 1 && kp == 2){
-                System.out.println("Laimejo popierius");
-                System.out.println("----------------------------");
-            }
-            if(choose == 1 && kp == 3){
-                System.out.println("Laimejo akmuo");
-                System.out.println("----------------------------");
-            }
-            if(choose == 2 && kp == 1){
-                System.out.println("Laimejo popierius");
-                System.out.println("----------------------------");
-            }
-            if(choose == 2 && kp == 3){
-                System.out.println("Laimejo zirkles");
-                System.out.println("----------------------------");
-            }
-            if(choose == 3 && kp == 1){
-                System.out.println("Laimejo akmuo");
-                System.out.println("----------------------------");
-            }
-            if(choose == 3 && kp == 2){
-                System.out.println("Laimejo zirkles");
-                System.out.println("----------------------------");
-            }
-        }
     }
-    
 }
